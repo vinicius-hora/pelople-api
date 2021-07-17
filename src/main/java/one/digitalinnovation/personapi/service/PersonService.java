9b25bc4e6cd7f746b15e99c2a0b049bd8791c5e5
vinicius-hora <vinicius_hora@live.com>
2021-07-17 12:34:14 -0300

    criação de pessoa

---
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -0,0 +1,29 @@
+package one.digitalinnovation.personapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+
+@Service
+public class PersonService {
+	
+	private PersonRepository personRepository;
+	
+	@Autowired
+	public PersonService(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
+	
+	public MessageResponseDTO createPerson(@RequestBody Person person) {
+		Person savedPerson = personRepository.save(person);
+		return MessageResponseDTO.builder().message("Created person with ID " + savedPerson.getId()).build();
+		
+	}
+	
+	
+
+}
